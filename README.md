fb51b5c4dd1669aaadc5616fabcb0a331626fbc9
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-02-08 08:00:39 -0300

    Estilização feita

---
+++ README.md
@@ -0,0 +1,13 @@
+## React Interview Challenge #04
+
+## Objetivos
+
+- crie um board de 3x3
+- dois jogadores
+- ao clicar em um quadrado, preencher com a jogada
+- avisar quando o jogo finalizar, caso dê velha avise também
+- fazer um risco na sequência vencedora, caso houver
+
+## Resolução do desafio
+
+https://youtu.be/PVCrq8rBXE4
---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./styles.scss";
+
+/*
+  DESAFIO TÉCNICO - JOGO DA VELHA - por fernandev
+
+  * descrição
+    desenvolva um jogo da velha (tic tac toe) funcional.
+    use qualquer técnica de estilização preferida: css modules, sass, styled.
+
+  * tasks
+    ? - crie um board de 3x3
+    ? - dois jogadores
+    ? - ao clicar em um quadrado, preencher com a jogada
+    ? - avisar quando o jogo finalizar, caso dê velha avise também
+    ? - fazer um risco na sequência vencedora, caso houver
+*/
+
+function App() {
+  const [gameData, setGameData] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [turn, setTurn] = useState(1);
+
+  const handleClick = (clickedIndex) => {
+    console.log(clickedIndex);
+
+    if (gameData[clickedIndex] !== 0) {
+      return;
+    }
+
+    setGameData((prev) => {
+      const newGameData = [...prev];
+      newGameData[clickedIndex] = turn;
+      return newGameData;
+    });
+
+    setTurn((prev) => (prev === 1 ? 2 : 1));
+  };
+
+  return (
+    <>
+      <div className="board-game">
+        {gameData.map((value, index) => (
+          <span
+            key={index}
+            onClick={() => {
+              handleClick(index);
+            }}
+          >
+            {value}
+          </span>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
